368410d6821eeb66f0446ba5ba2d0579cb96a0c2
Gabriel Carvalho <gabrielcpcontato@outlook.com>
2020-11-25 15:36:10 -0300

    Preauthorizing methods

---
+++ backend/src/main/java/com/gabriel/dslearnbds/services/DeliverService.java
@@ -0,0 +1,25 @@
+package com.gabriel.dslearnbds.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gabriel.dslearnbds.dto.DeliverRevisionDTO;
+import com.gabriel.dslearnbds.entities.Deliver;
+import com.gabriel.dslearnbds.repositories.DeliverRepository;
+
+@Service
+public class DeliverService {
+
+	@Autowired
+	private DeliverRepository repository;
+	
+	@Transactional
+	public void saveRevision(Long id, DeliverRevisionDTO dto) {
+		Deliver deliver = repository.getOne(id);
+		deliver.setStatus(dto.getStatus());
+		deliver.setFeedback(dto.getFeedback());
+		deliver.setCorrectCount(dto.getCorrectCount());
+		repository.save(deliver);
+	}
+}
